b3bd43f862559d868ffb2cd6264f865771229fb3
JacksonHouston <Houston.jack07@gmail.com>
2021-11-14 13:15:50 -0500

    Updated index.js and added the required png files for the goose memes
    added random cat pics
    added random goose memes

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Message } = require('discord.js');
 require('dotenv').config();
+
+const cheerio = require('cheerio');
+const request = require('request');
+const {get} = require("snekfetch");
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 client.on('ready', () => {
@@ -20,11 +25,63 @@ client.on('messageCreate', msg => {
         msg.channel.send('https://www.youtube.com/watch?v=n3DfLpdhXkg');
     }
     if (msg.content.toLowerCase().includes('meme')) {
-        var rand = Math.floor(Math.random() * 7) +1;
+        var rand = Math.floor(Math.random() * 13) +1;
         msg.channel.send({files: ["C:/Users/Houst/Documents/GitHub/LooseGoose/Memes/Meme" + rand + ".png"] });
     }
-});
+    if (msg.content.toLowerCase().includes('cat')) {
+        try {
+            get('https://aws.random.cat/meow').then(response => {
+                  msg.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[4]}`}]});
+                  console.log('random cat picture');
+                   })
+                   } catch (e) {
+                        console.log('error!');
+                        }
+        };
+        if (msg.content.toLowerCase().includes('super cat')) {
+            for(var i =0; i<10; i++){
+                try {
+                get('https://aws.random.cat/meow').then(response => {
+                      msg.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[4]}`}]});
+                      console.log('random cat picture');
+                       })
+                       } catch (e) {
+                            console.log('error!');
+                            }
+            };
+        }
+            
+    }
+);
 
 (async() => {
     client.login(process.env.token);
 })();
+
+function image(msg){
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "cat",
+        method: "GET",
+        header: {
+            "Accept" : "text/html",
+            "User-Agent" : "Chrome"
+        }
+    };
+
+    request(options, function(error, response, responseBody) {
+        if(error){
+            return;
+        }
+        $ = cheerio.load(responseBody);
+
+        var links = $(".image a.link");
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+
+        console.log(urls);
+        if(!urls.length){
+            return;
+        }
+
+        Message.channel.send(urls[Math.floor(Math.random() * urls.length)]);
+    });
+}
