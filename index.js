d8c92efdc1e4a27e92a3ef7d4d6ac5eef5bacbfb
JacksonHouston <houston.jack07@gmail.com>
2023-04-02 12:31:48 -0400

    updated 'help list' command to reflect previous changes

--- index.js
+++ index.js
@@ -359,7 +359,7 @@ client.on('messageCreate', msg => {
         }
         //HELP------------------------------------------- 
         if (message.includes('help list')) {
-            msg.channel.send("List Commands:\n > **'show list'**, to display all items active on the list\n > **'add [quantity] [foodname] to list'**\n > **'clear list'**, sets all items on list to 'inactive' and does not display them\n > **'remove [foodname] from list'**, removes single item from the list\n > **'delete [foodname] from list'**, permanently removes item from list\n > **'delete all from list'**, permanently deletes all items from the list\n");
+            msg.channel.send("List Commands:\n > **'show list'**, to display all items active on the list\n > **'add [quantity*] [foodname] to list'**, quantity is optional. if not specified it will default to 1\n > **'clear list'**, sets all items on list to 'inactive' and does not display them\n > **'remove [foodname] from list'**, removes single item from the list\n > **'delete [foodname] from list'**, permanently removes item from list\n > **'delete all from list'**, permanently deletes all items from the list\n");
         }
         //INVENTORY COMMANDS------------------------------------------- 
 
