b661dedd8bd50f7f83882e151e17aa6b40878b88
JacksonHouston <Houston.jack07@gmail.com>
2023-04-06 23:32:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -200,14 +200,14 @@ client.on('messageCreate', msg => {
             }
         }
         //ADD to List Table
-        if (message.includes('add') && message.includes('list') || message.includes('the')) {
+        if (message.includes('add') && message.includes('list')) {
             listItem = msg.content.split(" ");
             let Quantity = Number(listItem[1]); //get number of items
             let Food = '';
             //if number of items is not a number then set Quantity to 1 and concat Food starting at Quantity's index
             if (Number.isNaN(Quantity)) { 
                 //loop through array starting at second position until second to last
-                for (let i = 1; i < (listItem.length - 2); i++) {
+                for (let i = 1; i < (listItem.length); i++) {
                     //if item is 'to' break out and do not add it to string
                     if (listItem[i] === 'to') 
                         return;
@@ -217,7 +217,7 @@ client.on('messageCreate', msg => {
                 Quantity = 1;
             } else {
                 //concat Food starting after Quantity's index
-                for (let i = 2; i < (listItem.length - 2); i++) {
+                for (let i = 2; i < (listItem.length); i++) {
                     if (listItem === 'to') 
                         return;
                     else
